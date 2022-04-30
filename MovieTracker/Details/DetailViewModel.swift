bc35e0ee256e011e6d3fea779c43f44d986dc778
Azrua <a.killian@hotmail.fr>
2022-04-30 21:20:10 +0200

    UI

---
+++ MovieTracker/Details/DetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DetailViewModel.swift
+//  MovieTracker
+//
+//  Created by Killian Adonaï on 30/04/2022.
+//
+
+import Foundation
--- MovieTracker/Managers/NetworkManager.swift
+++ MovieTracker/Managers/NetworkManager.swift
@@ -6,24 +6,10 @@
 //
 
 import Foundation
-import CoreData
 
 struct NetworkManager {
 	static let shared = NetworkManager()
-
-	let container: NSPersistentContainer
-
-	init(inMemory: Bool = false) {
-		container = NSPersistentContainer(name: "MovieTracker")
-		if inMemory {
-			container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
-		}
-		container.viewContext.automaticallyMergesChangesFromParent = true
-		container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-			if let error = error as NSError? {
-				fatalError("Unresolved error \(error), \(error.userInfo)")
-			}
-		})
-	}
+	
+	
 }
 
